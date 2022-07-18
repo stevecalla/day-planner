b3d0c2b874cbc5d27681ff3a4404c21573cac417
Steve Calla <callasteven@gmail.com>
2022-07-18 15:42:54 -0600

    Format time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
 let date = new Date;
 let currentDay = $('#currentDay');
 currentDay.text(date);
+
+
+//section:query selector variables go here 👇
+let timeBlockContainer = $('#container');
+
+//section:global variables go here 👇
+
+//create timeblocks
+// let hourOfDay = "<p>";
+// let event = "<input>";
+// let saveButton = "<p>";
+
+//section:event listeners go here 👇
+
+
+//section:functions and event handlers go here 👇
