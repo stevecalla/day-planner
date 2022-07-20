3e3d4b8af8692bc216e6614f2afedba48f031708
Steve Calla <callasteven@gmail.com>
2022-07-19 18:04:38 -0600

    Refactor and remove duplicate jQuery scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,36 @@
 //query selector va*riables go here 👇
 let currentDay = $('#currentDay');
 let saveIconButton = $('*#save-button');
-let activityInput = $('*#activityInput');
+let activityInputText = $('*#activityInput');
 // console.log(activityInput)
 
 //global variables go here 👇
-// let date = new Date;
 
 //event listeners go here 👇
 $(saveIconButton).on('click', saveActivity)
 
-//jQuery Interactions
-// $('.alert').alert(close); //jQuery to hide the alert buttons on click of the X
+//jQuery Interactions 👇
 
 //functions and event handlers go here 👇
-
 // ON LOAD FUNCTIONS
 $(document).ready(function() {
-  console.log( "document loaded" );
   let activitiesFromLocalStorage = getFromStorage();
-  renderHeaderDateTime();
-  renderStorageToTextArea(activitiesFromLocalStorage);
-  renderActivityStyle();
+  renderDateTimeInHeader();
+  renderLocalStorageToTextArea(activitiesFromLocalStorage);
+  renderTextAreaStyle();
 
-  // let checkHour = setInterval(() => {
-  //   setHeaderDateTime();
-  //   renderActivityStyle();
-  // }, 1000);
-  // clearInterval(checkHour);
-
-  // $( window ).on( "load", function() {
-  //     console.log( "window loaded" );
-  // });
+  let checkHour = setInterval(() => {
+    renderDateTimeInHeader();
+    renderTextAreaStyle();
+  }, 1000);
 });
 
-function renderHeaderDateTime() {
+function renderDateTimeInHeader() {
   currentDay.text(moment().format("dddd, MMMM Do [at] h:mm:ss a"))
 }
 
-function renderStorageToTextArea(activitiesFromLocalStorage) {
-  activityInput.each( function(index, element) {
+function renderLocalStorageToTextArea(activitiesFromLocalStorage) {
+  activityInputText.each( function(index, element) {
     $(element).val(activitiesFromLocalStorage[index]);
   });
 }
@@ -47,25 +38,18 @@ function renderStorageToTextArea(activitiesFromLocalStorage) {
 // SAVE BUTTON
 function saveActivity(event) {
   if ($('#alert').hasClass("cloak")) { //prevents multiple clicks thus timers while alert is displayed
-    // console.log($(event.target).attr('data-hour'));
     saveTargetToLocalStorage(event);
-    // saveAllToLocalStorage(event);
     toggleAlertVisibility();
-    renderAlertMessageAndIcon(event);
-    renderSaveIconCheckmark(event)
-    hideAlert();
-    hideSaveIconCheckmark(event);
-    // renderActivityStyle(event);
+    renderSavedIconCheckmark(event)
+    hideSavedAlertAndCheckmarkIcon(event);
   }
-  // $(saveIconButton).off("click");
 }
 
 // RENDER ACTIVITY PAST, PRESENT, FUTURE STYLE
-function renderActivityStyle() {
+function renderTextAreaStyle() {
   let dateNow = moment(); //determine current date
   let date_time = "";
-  // console.log(moment(dateNow).format("hh:mm:ss"));
-  activityInput.each(function( index, element ) {
+  activityInputText.each(function(index, element) {
     date_time = moment(dateNow).set({'hour': $(element).attr('data-hour'), 'minute': $(element).attr('data-minutes')}); //append hour to current date 
 
     //if hour before current hour style grey, if hour same as current hour style red else style green
@@ -80,43 +64,23 @@ function toggleAlertVisibility() {
   $('#alert').toggleClass('cloak');
 }
 
-function renderAlertMessageAndIcon() {
-  $('#alert').text(`Appointment Saved to localStorage ✅`);
-
-  // $(event.target).attr("data-status") === 'not-saved' ? (
-  //   $(event.target).attr("data-status", "saved"),
-  //   $('#alert').text(`Appointment Saved to localStorage ✅`)
-  //   //save to local storage
-  //   ) : 
-  //   ($(event.target).attr("data-status", "not-saved"),
-  //     $('#alert').text(`Appointment Removed from localStorage ❌`
-  //     //remove from local storage
-  //     )
-  //   )
-}
-
-function renderSaveIconCheckmark(event) {
+function renderSavedIconCheckmark(event) {
   $(event.target).attr('src', "./assets/images/save-checkmark.png");
 }
 
-function hideAlert(event) {
+function hideSavedAlertAndCheckmarkIcon(event) {
   setTimeout(() => {
     toggleAlertVisibility();
-  }, 500);
-}
-
-function hideSaveIconCheckmark(event) {
-  setTimeout(() => {
     $(event.target).attr('src', "./assets/images/save-no-checkmark.png");
-  }, 500);
+  }, 1000);
 }
 
 // LOCAL STORAGE FUNCTIONS
 function saveTargetToLocalStorage(event) {
   activityList = getFromStorage();
 
-  activityInput.each( function(index, element) {
-    console.log($(event.target).attr("data-hour"), $(element).attr('data-hour'));
+  activityInputText.each( function(index, element) {
+    // console.log($(event.target).attr("data-hour"), $(element).attr('data-hour'));
     activity = $(element).val();
     if ($(event.target).attr("data-hour") === $(element).attr('data-hour') && activityList.length > 0) {
       //replace activity list with input value
@@ -128,15 +92,6 @@ function saveTargetToLocalStorage(event) {
   });
   localStorage.setItem('dayPlannerActivities', JSON.stringify(activityList));
 }
-
-// function saveAllToLocalStorage(event) {
-  // activityInput.each( function(index, element) {
-  //   activity = $(element).val();
-  //   console.log(activity, $(element).val());
-  //   activityList[index] = activity;
-  //   localStorage.setItem('dayPlannerActivities', JSON.stringify(activityList));
-  // });
-// }
 
 function removeFromStoraget() {
   // TBD
@@ -150,8 +105,5 @@ function getFromStorage() {
 
   return activityList; 
 }
-
-
-
 
 
